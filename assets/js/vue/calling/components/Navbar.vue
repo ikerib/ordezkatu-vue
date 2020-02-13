6be8c37b588e75b5a2b1a4c5b6b96d5bed6e7d2d
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2020-02-13 09:25:50 +0100

    Filter select inline

--- assets/js/vue/calling/components/Navbar.vue
+++ assets/js/vue/calling/components/Navbar.vue
@@ -1,21 +1,22 @@
 <template>
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
-        <div class="row container">
-            <div class="col-md-4"><h5><strong>Eskeintza</strong>: {{ jobName }}</h5></div>
-            <div class="col-md-4"></div>
-            <div class="col-md-4">
-                <label for="filterByCallStatus">Filtratu</label>
-                <select :value="filterCallStatus" @input="doFilterCallStatusChange" id="filterByCallStatus" class="custom-select">
-                    <option value="-1">Guztiak</option>
-                    <option value="0">Deitu gabe</option>
-                    <option v-for="erantzuna in erantzunak" v-bind:value="erantzuna.id">{{erantzuna.name}}</option>
-                </select>
+<!--    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">-->
+        <div class="row navbar-light" style="background-color: #e3f2fd;">
+            <div class="col-8"><h1>Eskeintza: {{ jobName }}</h1></div>
+            <div class="col-4">
+
+<!--                    <div class="form-group">-->
+
+                        <label class="d-inline-block" for="filterByCallStatus">Filtratu</label>
+                        <select :value="filterCallStatus" @input="doFilterCallStatusChange" id="filterByCallStatus" class="form-control custom-select d-inline-block">
+                            <option value="-1">Guztiak</option>
+                            <option value="0">Deitu gabe</option>
+                            <option v-for="erantzuna in erantzunak" v-bind:value="erantzuna.id">{{erantzuna.name}}</option>
+                        </select>
+
+<!--                    </div>-->
+
             </div>
         </div>
-<!--        <div class="col-md-10">-->
-<!--            <h5>// </h5>-->
-<!--        </div>-->
-    </nav>
 </template>
 
 <script>
@@ -50,3 +51,34 @@
     }
 
 </script>
+
+<style scoped>
+    select {
+        padding-left:65px;
+        height:40px;
+        position:relative;
+        border:1px solid #333;
+        width:300px;
+        display:block;
+        appearance: none;
+        -webkit-appearance: none;
+        -moz-appearance: none;
+        -o-appearance: none;
+        -ms-appearance: none;
+        border-radius: 0;
+        -webkit-box-shadow: none;
+    }
+    label {
+        position:absolute;
+        z-index:1;
+        height:40px;
+        line-height:40px;
+        pointer-events:none;
+        text-indent:10px;
+        font-weight:bold;
+    }
+    label, select {
+        font-family:Arial;
+        font-size:15px;
+    }
+</style>
